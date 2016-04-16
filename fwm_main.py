3320890653eaa182b632a5dafc12739f332ad3cc
celisoft <celine.liberal@mail.com>
2016-04-16 19:07:50 +0200

    GameTile using class + game title updated

--- fwm_main.py
+++ fwm_main.py
@@ -4,6 +4,8 @@ import pygame
 import pytmx
 from pygame.locals import *
 
+from GameTile import GameTile
+
 
 class FWMMain():
     def __init__(self):
@@ -18,29 +20,22 @@ class FWMMain():
         # Clock
         self.clock = pygame.time.Clock()
 
-        self.screen = pygame.display.set_mode((64*18, 64*12))
-        pygame.display.set_caption('Hello Pygame World!')
+        self.screen = pygame.display.set_mode((64 * 18, 64 * 12))
+        pygame.display.set_caption('Frozen Water Maze')
 
         # Load data
         tmxdata = pytmx.load_pygame("assets/map.tmx")
-        map_tiles = []
+        game_tiles = []
         for coord_x in range(0, 18):
-            y_tiles = []
             for coord_y in range(0, 12):
                 img = tmxdata.get_tile_image(coord_x, coord_y, 0)
                 if img is not None:
-                    img_rect = img.get_rect().copy()
-                    img_rect.left = coord_x*64
-                    img_rect.top = coord_y*64
-                    y_tiles.append([img, img_rect])
-            map_tiles.append(y_tiles)
-            del y_tiles
+                    game_tiles.append(GameTile(img, coord_x, coord_y))
 
         # Game loop
         while not self.game_ended:
-            for map_line in map_tiles:
-                for img in map_line:
-                    self.screen.blit(img[0], img[1])
+            for tile in game_tiles:
+                tile.display(self.screen)
             pygame.time.wait(50)
             self.clock.tick(60)
             pygame.display.flip()
