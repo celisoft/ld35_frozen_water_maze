ecc2ee0bcfd66de9f9492ca57f6a515410c2a05e
celisoft <celine.liberal@mail.com>
2016-04-17 12:35:35 +0200

    Music / SFX playing OK

--- fwm_main.py
+++ fwm_main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 import pygame
 import pytmx
@@ -33,11 +34,32 @@ class FWMMain():
                 if img is not None:
                     game_tiles.append(GameTile(img, coord_x, coord_y))
 
+        # Init music
+        music_path = os.path.dirname(__file__) + os.sep + "assets/sfx/bg_music.ogg"
+        pygame.mixer.music.load(music_path)
+        pygame.mixer.music.set_volume(0.25)
+        pygame.mixer.music.play(-1)
+
+        # Init ambient motor sound
+        ambient_motor_path = os.path.dirname(__file__) + os.sep + "assets/sfx/motor.ogg"
+        ambient_motor = pygame.mixer.Sound(ambient_motor_path)
+        ambient_motor.set_volume(0.15)
+        ambient_motor.play(loops=-1)
+
+        # Init ambient droplet fall sound
+        droplet_path = os.path.dirname(__file__) + os.sep + "assets/sfx/droplet.ogg"
+        self.ambient_droplet = pygame.mixer.Sound(droplet_path)
+        self.ambient_droplet.set_volume(0.3)
+
         # Init player
         player = Player()
 
+        # Init internal event -> droplet fall
+        pygame.time.set_timer(pygame.USEREVENT, 5000)
+
         # Game loop
         while not self.game_ended:
+            self.check_game_event()
             for tile in game_tiles:
                 tile.display(self.screen)
             player.display(self.screen)
@@ -46,6 +68,13 @@ class FWMMain():
             self.clock.tick(60)
             pygame.display.flip()
 
+        pygame.quit()
+
+    def check_game_event(self):
+        """ Check game events """
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:
+                self.ambient_droplet.play()
 
 if __name__ == "__main__":
     FWMMain()
